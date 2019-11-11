0d5d08848dc4a6539f466672362a22ba5ace5c52
MonsterRHD <renhuangding@gmail.com>
2019-11-11 22:09:08 +0800

    新增不是模块写法的小程序倒计时

---
+++ new.js
@@ -0,0 +1,63 @@
+// 执行倒计时状态
+          that.startSetInter("2019-11-11 22:14:00")
+  Page({
+  data: {
+      //存储计时器
+     
+    interval:'',//存储计时器
+  },
+  onLoad: function () {
+      var that = this;
+       
+  },
+  startSetInter: function (endTime) {
+    var that = this;
+    var countdownId = 'countdown'
+    let end = new Date(endTime).getTime()
+    that.setData({
+      [countdownId]: {
+        countdown: parseInt((end - new Date().getTime()) / 1000),
+        day: parseInt((end - new Date().getTime()) / 1000 / 60 / 60 / 24),
+        hour: parseInt((end - new Date().getTime()) / 1000 / 60 / 60 % 24),
+        minute: parseInt((end - new Date().getTime()) / 1000 / 60 % 60),
+        seconds: parseInt((end - new Date().getTime()) / 1000) % 60
+      }
+    })
+    that.data.interval = setInterval(() => {
+      
+      that.setData({
+        [countdownId]: {
+          countdown: parseInt((end - new Date().getTime()) / 1000),
+          day: parseInt((end - new Date().getTime()) / 1000 / 60 / 60 / 24),
+          hour: parseInt((end - new Date().getTime()) / 1000 / 60 / 60 % 24),
+          minute: parseInt((end - new Date().getTime()) / 1000 / 60 % 60),
+          seconds: parseInt((end - new Date().getTime()) / 1000) % 60
+        }
+      })
+      
+      if (that.data[countdownId].countdown <= 0) {
+        clearInterval(interval)
+        that.setData({
+          seconds: 0
+        })
+        // 执行任务
+        that.postFoodMission()
+      }
+
+    }, 1000)
+  },
+  endSetInter: function() {
+    var that = this;
+    //清除计时器  即清除setInter
+    clearInterval(that.data.interval)
+  },
+  onHide: function () {
+ 
+  },
+  onUnload: function () {
+      var that =this;
+      //清除计时器  即清除setInter
+    clearInterval(that.data.interval)
+  },
+ 
+})
